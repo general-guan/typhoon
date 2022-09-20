efa65bb9001845e4b28f434de937c2cf3e9bb827
gjj <934750771>
2022-09-20 22:59:17 +0800

    chore: 引入 prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 一行代码的最大字符数，默认是 80（printWidth: <int>）
+  printWidth: 80,
+  // tab 宽度为 2 空格（tabWidth: <int>）
+  tabWidth: 2,
+  // 是否使用 tab 来缩进，我们使用空格（useTabs: <bool>）
+  useTabs: false,
+  // 结尾是否添加分号，只在可能导致ASI故障的行开头添加分号（semi: <bool>）
+  semi: false,
+  // 是否使用单引号（singleQuote: <bool>）
+  singleQuote: true,
+  // 对象中的 key 是否加引号，只在需要的地方在对象属性周围添加引号（quoteProps: "<as-needed|consistent|preserve>"）
+  quoteProps: 'as-needed',
+}
