5f2ba6ebf63197d68320962f0051e9f34967f88c
guanCompany <934750771@qq.com>
2022-09-26 16:13:33 +0800

    chore: 引入 electron 组件间通信

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
+import electronRender from 'vite-plugin-electron-renderer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
         entry: 'electron/index.ts',
       },
     }),
+    electronRender(),
   ],
 })
