6b664470138d6a90dad4881217e75afbde79cf4d
guanCompany <934750771@qq.com>
2022-09-26 19:50:57 +0800

    feat: 打开文件夹 打开文件

--- electron/menu.ts
+++ electron/menu.ts
@@ -1,4 +1,4 @@
-import { app, Menu, shell } from 'electron'
+import { Menu, BrowserWindow, dialog } from 'electron'
 import type { MenuItemConstructorOptions } from 'electron'
 
 const isMac = process.platform === 'darwin'
@@ -7,20 +7,21 @@ const template: MenuItemConstructorOptions[] = [
   {
     label: '文件',
     submenu: [
+      {
+        label: '打开文件夹',
+        click: async (menuItem, browserWindow) => {
+          const { filePaths } = await dialog.showOpenDialog(
+            browserWindow as BrowserWindow,
+            { properties: ['openDirectory'] }
+          )
+          if (filePaths[0]) {
+            browserWindow?.webContents.send('openFolder', filePaths[0])
+          }
+        },
+      },
       isMac
         ? { label: '退出', role: 'close' }
         : { label: '退出', role: 'quit' },
-    ],
-  },
-  {
-    label: '帮助',
-    submenu: [
-      {
-        label: '今晚打老虎',
-        click: async () => {
-          await shell.openExternal('https://www.baidu.com')
-        },
-      },
     ],
   },
 ]
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,74 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>123456</div>
+  <div
+    style="display: flex; height: 100vh"
+    @mousemove="splitLineMouseMove"
+    @mouseup="splitLineMouseUp"
+  >
+    <ul style="flex-shrink: 0" :style="{ width: ulWidth + 'px' }">
+      <li v-for="item in fileList" @click="openFiles(item)">{{ item }}</li>
+    </ul>
+
+    <div
+      ref="splitLineRef"
+      class="splitLine"
+      @mousedown="splitLineMouseDown"
+      style="flex-shrink: 0"
+      v-if="fileList.length"
+    ></div>
+
+    <div style="height: 100%; overflow: auto">
+      {{ content }}
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ipcRenderer } from 'electron'
+import { ref } from 'vue'
+import fs from 'fs'
+
+// 文件列表
+const root = ref<string>('')
+const fileList = ref<string[]>([])
+const content = ref<string>('')
+const ulWidth = ref<number>(300)
+const splitLineIsFocus = ref<boolean>(false)
+const splitLineRef = ref(null)
+
+ipcRenderer.on('openFolder', (_, data) => {
+  root.value = data
+  fileList.value = fs.readdirSync(data)
+})
+
+const openFiles = (item: string) => {
+  fs.readFile(root.value + '\\' + item, (err, data) => {
+    content.value = data.toString()
+  })
+}
+
+const splitLineMouseDown = (e) => {
+  splitLineIsFocus.value = true
+  // console.log(e)
+}
+const splitLineMouseMove = (e) => {
+  if (splitLineIsFocus.value) {
+    ulWidth.value = e.clientX - splitLineRef.value.offsetWidth / 2
+
+    console.log(ulWidth.value)
+  }
+}
+const splitLineMouseUp = (e) => {
+  splitLineIsFocus.value = false
+}
+</script>
+
+<style>
+.splitLine {
+  width: 20px;
+  background-color: #ccc;
+  height: 100%;
+}
+.splitLine:hover {
+  cursor: ew-resize;
+}
+</style>
