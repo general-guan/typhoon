b6ecf9c102f58b8773df98485b71b70ebb1bb54f
guanCompany <934750771@qq.com>
2022-09-27 20:27:41 +0800

    feat: 分割线移动

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div
+    ref="containRef"
     style="display: flex; height: 100vh"
-    @mousemove="splitLineMouseMove"
     @mouseup="splitLineMouseUp"
+    @mousemove="throttleSplitLineMouseMove"
   >
-    <ul style="flex-shrink: 0" :style="{ width: ulWidth + 'px' }">
+    <ul
+      style="flex-shrink: 0; background-color: #f3feb0"
+      :style="{ width: ulWidth + 'px' }"
+    >
       <li v-for="item in fileList" @click="openFiles(item)">{{ item }}</li>
     </ul>
 
     <div
-      ref="splitLineRef"
-      class="splitLine"
+      class="splitLineContainer"
+      ref="splitLineContainerRef"
       @mousedown="splitLineMouseDown"
       style="flex-shrink: 0"
-      v-if="fileList.length"
-    ></div>
+    >
+      <div
+        class="splitLine"
+        :class="{ splitLineMoveClass: splitLineIsFocus }"
+      ></div>
+    </div>
 
-    <div style="height: 100%; overflow: auto">
+    <div style="height: 100%; overflow: auto; padding: 20px 30px">
       {{ content }}
     </div>
   </div>
@@ -26,6 +34,7 @@
 import { ipcRenderer } from 'electron'
 import { ref } from 'vue'
 import fs from 'fs'
+import { throttle } from './utils'
 
 // 文件列表
 const root = ref<string>('')
@@ -33,7 +42,8 @@ const fileList = ref<string[]>([])
 const content = ref<string>('')
 const ulWidth = ref<number>(300)
 const splitLineIsFocus = ref<boolean>(false)
-const splitLineRef = ref(null)
+const splitLineContainerRef = ref()
+const containRef = ref()
 
 ipcRenderer.on('openFolder', (_, data) => {
   root.value = data
@@ -43,32 +53,61 @@ ipcRenderer.on('openFolder', (_, data) => {
 const openFiles = (item: string) => {
   fs.readFile(root.value + '\\' + item, (err, data) => {
     content.value = data.toString()
+    console.log(content.value)
   })
 }
 
-const splitLineMouseDown = (e) => {
+/**
+ * 分割线鼠标按下
+ */
+const splitLineMouseDown = () => {
   splitLineIsFocus.value = true
-  // console.log(e)
+  containRef.value.style.userSelect = 'none'
 }
-const splitLineMouseMove = (e) => {
-  if (splitLineIsFocus.value) {
-    ulWidth.value = e.clientX - splitLineRef.value.offsetWidth / 2
 
-    console.log(ulWidth.value)
+/**
+ * 分割线移动
+ */
+const splitLineMouseMove = (e: MouseEvent) => {
+  if (splitLineIsFocus.value) {
+    ulWidth.value = e.clientX - splitLineContainerRef.value.offsetWidth / 2
   }
 }
-const splitLineMouseUp = (e) => {
+
+/**
+ * 移动节流
+ */
+const throttleSplitLineMouseMove = throttle(splitLineMouseMove, 20, {
+  leading: true,
+})
+
+/**
+ * 分割线鼠标抬起
+ */
+const splitLineMouseUp = () => {
   splitLineIsFocus.value = false
+  containRef.value.style.userSelect = ''
 }
 </script>
 
 <style>
+.splitLineContainer {
+  width: 4px;
+  cursor: ew-resize;
+}
+/* TODO deley */
+.splitLineContainer:hover .splitLine {
+  background-color: #1871ff;
+  width: 4px;
+}
 .splitLine {
-  width: 20px;
-  background-color: #ccc;
+  width: 1px;
+  margin: 0 auto;
+  background-color: #cacdd1;
   height: 100%;
 }
-.splitLine:hover {
-  cursor: ew-resize;
+.splitLine.splitLineMoveClass {
+  background-color: #1871ff;
+  width: 4px;
 }
 </style>
